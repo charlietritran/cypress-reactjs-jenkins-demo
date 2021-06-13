b60d46a74ceb829a7a5e3c53e49abb60b24255f9
Charlie.Tran@leidos.com <Charlie.Tran@leidos.com>
2021-06-12 21:14:26 -0700

    added intercept

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,34 @@
+
+import React from 'react'
+
+
 describe('The Home Page', () => {
   it('successfully loads', () => {
+
+    cy.log('START');
+    cy.intercept('/citmData.json', (req) => {
+
+      req.reply((res) => {
+
+        // update msg
+        console.log('RES:', res.body)
+        let jsonObj = res.body
+        jsonObj.msg = 'Welcome to Cypress Intercept12345' 
+
+
+        //list.push('Kiwi')
+        res.send(jsonObj)
+      })
+    })
+
+    //cy.intercept('/citmData.json', { msg: 'Welcome to Cypress Intercept' })
     cy.visit('/'); // change URL to match your dev URL
+    
   });
+
+
+
+    
+
+
 });
--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppService from './services/AppService.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.appService = new AppService();
+
+    // Initialize App state
+    this.state = {
+      mode: '',
+      msg: ''
+    }
+  }
+
+
+
+  async componentDidMount() {
+
+    // Call service to add  new entry to db
+    const data = await this.appService.getCitmDataJson('citmData.json');
+    console.log('SERVICE RETURN:', data);
+    if (data) {
+      console.log('msg: ', data.msg);
+      this.setState({mode: data.mode})
+      this.setState({msg: data.msg})
+
+    }
+
+  }
   render() {
     return (
       <div className="App">
@@ -12,6 +41,12 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          MODE IS: {this.state.mode}
+        </p>
+        <p>
+          MESSAGE IS: {this.state.msg}
         </p>
       </div>
     );
