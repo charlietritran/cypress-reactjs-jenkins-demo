7243f740bb601e50262f9d6d8fdfb440fecd18f2
Charlie.Tran@leidos.com <Charlie.Tran@leidos.com>
2021-06-14 07:33:46 -0700

    missed services

---
+++ src/services/AppService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+class AppService {
+
+  constructor() {}
+
+  /**
+   * GET CITM INIT DATA IN JSON FORMAT
+   * RETURN: a json data for citm-app 
+   * @param {*} url 
+   */
+  async getCitmDataJson(url) {
+
+    console.log("@@@ ===> GET CitmJsonData STARTS - URL:" + url );
+    try {
+      const result = await axios.get(url);
+
+      console.log('SUCCESS');
+      return result.data;
+
+    } catch (error) {
+
+      console.log('GET citmDataJson ERROR:' + error.message);
+      throw error;
+    }
+  }
+
+}
+
+export default AppService;
